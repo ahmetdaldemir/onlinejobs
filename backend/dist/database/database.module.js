ddf2e802b13d685e6b3d3f052ee784297c857520
Ahmet DALDEMİR <21971399+ahmetdaldemir@users.noreply.github.com>
2025-07-21 20:57:15 +0300

    updated

--- backend/dist/database/database.module.js
+++ backend/dist/database/database.module.js
@@ -37,7 +37,7 @@ exports.DatabaseModule = DatabaseModule = __decorate([
                             type: 'postgres',
                             host: configService.get('DATABASE_HOST', 'localhost'),
                             port: configService.get('DATABASE_PORT', 5432),
-                            username: configService.get('DATABASE_USERNAME', 'postgres'),
+                            username: configService.get('DATABASE_USERNAME', 'neondb_owner'),
                             password: configService.get('DATABASE_PASSWORD', 'password123'),
                             database: configService.get('DATABASE_NAME', 'onlinejobs'),
                             entities: [__dirname + '/../**/*.entity{.ts,.js}'],
--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -27,7 +27,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
             type: 'postgres',
             host: configService.get('DATABASE_HOST', 'localhost'),
             port: configService.get('DATABASE_PORT', 5432),
-            username: configService.get('DATABASE_USERNAME', 'postgres'),
+            username: configService.get('DATABASE_USERNAME', 'neondb_owner'),
             password: configService.get('DATABASE_PASSWORD', 'password123'),
             database: configService.get('DATABASE_NAME', 'onlinejobs'),
             entities: [__dirname + '/../**/*.entity{.ts,.js}'],
